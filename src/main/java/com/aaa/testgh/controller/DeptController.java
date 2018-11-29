8c96d8d03825c65d8d829ca8c3e1339e49585034
mx1996one <741200631@qq.com>
2018-11-29 09:32:57 +0800

    第一次提交

---
+++ src/main/java/com/aaa/testgh/controller/DeptController.java
@@ -0,0 +1,14 @@
+package com.aaa.testgh.controller;
+
+/**
+ * className:DeptController
+ * discription:
+ * author:mx
+ * createTime:2018-11-29 09:07
+ */
+
+public class DeptController {
+    public static void main(String[] args) {
+        System.out.println("测试GitHub提交,第一次");
+    }
+}
